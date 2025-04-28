2727810ed48d63fe83caa45e9edb6d9266b7172f
Khoa4chuwxA <dangkhoavochau@gmail.com>
2025-04-28 13:27:07 +0700

    Change format log and version requirements

--- src/utils/log_writer.py
+++ src/utils/log_writer.py
@@ -6,7 +6,9 @@ def write_log(pipeline_stage: str, step_id: str, run_date: str, message: str):
     log_dir = Path(f"src/logs/{pipeline_stage}/{step_id}")
     log_dir.mkdir(parents=True, exist_ok=True)
     log_path = log_dir / f"{run_date}.log"
-
-    timestamp = datetime.now(timezone(timedelta(hours=7))).isoformat()
+    
+    tz = timezone(timedelta(hours=7))
+    timestamp = datetime.now(tz).strftime("%Y-%m-%d, %H:%M:%S")
+    
     with open(log_path, "a", encoding="utf-8") as f:
         f.write(f"[{timestamp}] {message}\n")
